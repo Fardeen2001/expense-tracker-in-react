94d97850bf2cccb0cc06edcefce93d9bd8bae7af
Fardeen2001 <1si20et403@sit.ac.in>
2023-06-24 19:37:29 +0530

    dynamic Expenditure of location added

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -2,12 +2,14 @@ import "./ExpenseItems.css";
 function ExpenseItems() {
   const ExpenseDate = new Date(2023, 6, 24);
   const ExpenseName = "Food";
+  const LocationOfExpenditure = "Tumkur";
   const ExpensePrice = 10;
   return (
     <div className="expense-item">
       <div>{ExpenseDate.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{ExpenseName}</h2>
+        <div className="expense-item__location">{LocationOfExpenditure}</div>
         <div className="expense-item__price">Rs{ExpensePrice}</div>
       </div>
     </div>
