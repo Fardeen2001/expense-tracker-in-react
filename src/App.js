996fc6b033ab1f3327d9b5463b76b698ff781bbf
Fardeen2001 <1si20et403@sit.ac.in>
2023-06-25 08:23:44 +0530

    Dynamically data outputed from array through .map function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
 import ExpenseItems from "./components/ExpenseItems";
 
 function App() {
+  let Expenses = [
+    {
+      ExpenseName: "Petrol",
+      ExpensePrice: 100,
+      ExpenseDate: new Date(),
+      LocationOfExpenditure: "Tumkur",
+    },
+    {
+      ExpenseName: "Food",
+      ExpensePrice: 200,
+      ExpenseDate: new Date(),
+      LocationOfExpenditure: "Tumkur",
+    },
+    {
+      ExpenseName: "Birthday party",
+      ExpensePrice: 1000,
+      ExpenseDate: new Date(),
+      LocationOfExpenditure: "Tumkur",
+    },
+    {
+      ExpenseName: "Petrol",
+      ExpensePrice: 100,
+      ExpenseDate: new Date(2023, 5, 25),
+      LocationOfExpenditure: "Tumkur",
+    },
+    {
+      ExpenseName: "Lost",
+      ExpensePrice: 100,
+      ExpenseDate: new Date(2023, 5, 25),
+      LocationOfExpenditure: "Tumkur",
+    },
+  ];
   return (
     <div>
       <h2>Expense Tracker</h2>
-      <ExpenseItems />
+      {Expenses.map((ExpenseList, index) => {
+        return (
+          <ExpenseItems
+            ExpenseName={ExpenseList.ExpenseName}
+            ExpensePrice={ExpenseList.ExpensePrice}
+            ExpenseDate={ExpenseList.ExpenseDate}
+            LocationOfExpenditure={ExpenseList.LocationOfExpenditure}
+          />
+        );
+      })}
+      ;
+      {/* <ExpenseItems
+        ExpenseName={Expenses[1].ExpenseName}
+        ExpensePrice={Expenses[1].ExpensePrice}
+        ExpenseDate={Expenses[1].ExpenseDate}
+        LocationOfExpenditure={Expenses[1].LocationOfExpenditure}
+      />
+      <ExpenseItems
+        ExpenseName={Expenses[2].ExpenseName}
+        ExpensePrice={Expenses[2].ExpensePrice}
+        ExpenseDate={Expenses[2].ExpenseDate}
+        LocationOfExpenditure={Expenses[2].LocationOfExpenditure}
+      />
+      <ExpenseItems
+        ExpenseName={Expenses[3].ExpenseName}
+        ExpensePrice={Expenses[3].ExpensePrice}
+        ExpenseDate={Expenses[3].ExpenseDate}
+        LocationOfExpenditure={Expenses[3].LocationOfExpenditure}
+      /> */}
     </div>
   );
 }
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,16 +1,14 @@
 import "./ExpenseItems.css";
-function ExpenseItems() {
-  const ExpenseDate = new Date(2023, 6, 24);
-  const ExpenseName = "Food";
-  const LocationOfExpenditure = "Tumkur";
-  const ExpensePrice = 10;
+function ExpenseItems(props) {
   return (
     <div className="expense-item">
-      <div>{ExpenseDate.toISOString()}</div>
+      <div>{props.ExpenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{ExpenseName}</h2>
-        <div className="expense-item__location">{LocationOfExpenditure}</div>
-        <div className="expense-item__price">Rs{ExpensePrice}</div>
+        <h2>{props.ExpenseName}</h2>
+        <div className="expense-item__location">
+          {props.LocationOfExpenditure}
+        </div>
+        <div className="expense-item__price">Rs{props.ExpensePrice}</div>
       </div>
     </div>
   );
